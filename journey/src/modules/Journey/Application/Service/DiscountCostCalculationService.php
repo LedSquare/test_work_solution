81c8f441f21391219b4ca90cbd0605fd63ade8ad
LedSquare <exilelovich@yandex.ru>
2024-10-26 16:12:42 +0300

    Fea: Added stub date intervals for travel and payment

--- journey/src/modules/Journey/Application/Service/DiscountCostCalculationService.php
+++ journey/src/modules/Journey/Application/Service/DiscountCostCalculationService.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace App\modules\Journey\Application\Service;
+namespace Journey\Application\Service;
 
-use App\modules\Journey\Application\DTORequest\CostCalculationResponseDTO;
-use App\modules\Journey\Application\Service\DiscountCostCalculationServiceInterface;
-use App\modules\Journey\Domain\DTORequest\CostCalculationRequestDTOInterface;
-use App\modules\Journey\Domain\DTOResponse\CostCalculationResponseDTOInterface;
-use App\modules\Journey\Domain\Discount\DateIntervals\StartTravelDateInterval;
-use App\modules\Journey\Domain\Service\Discount\DiscountCalculateServiceInterface;
 use DateTime;
+use Journey\Application\DTORequest\CostCalculationResponseDTO;
+use Journey\Application\Service\DiscountCostCalculationServiceInterface;
+use Journey\Domain\DTORequest\CostCalculationRequestDTOInterface;
+use Journey\Domain\DTOResponse\CostCalculationResponseDTOInterface;
+use Journey\Domain\Discount\DateIntervals\PaymentDateInterval;
+use Journey\Domain\Discount\DateIntervals\StartTravelDateInterval;
+use Journey\Domain\Service\Discount\DiscountCalculateServiceInterface;
 
 final class DiscountCostCalculationService implements DiscountCostCalculationServiceInterface
 {
@@ -19,7 +20,11 @@ final class DiscountCostCalculationService implements DiscountCostCalculationSer
 
     public function calculate(CostCalculationRequestDTOInterface $requestDTO): CostCalculationResponseDTOInterface
     {
-        $discount = $this->discountCalculateService->calculate(requestDTO: $requestDTO);
+        $intervals = $this->setStartTravelAndPaymentDateIntervals();
+        $discount = $this->discountCalculateService->calculate(
+            requestDTO: $requestDTO,
+            startTravelDateIntervals: $intervals
+        );
 
         return new CostCalculationResponseDTO(
             discount: $discount
@@ -27,24 +32,84 @@ final class DiscountCostCalculationService implements DiscountCostCalculationSer
     }
 
     /**
-     * @var array<\App\modules\Journey\Domain\Discount\DateIntervals\StartTravelDateInterval>
+     * @var array<\Journey\Domain\Discount\DateIntervals\StartTravelDateInterval>
      */
-    public function setStartTravelAndPaymentDates(): array
+    private function setStartTravelAndPaymentDateIntervals(): array
     {
         $nowDate = new DateTime();
         $currentYear = $nowDate->format('Y');
 
         $newNowDate = new DateTime();
         $nextYear = $newNowDate->modify('+1 year')->format('Y');
-        // Допусти получили эти интервали типо из базы
+        // Допустим получили эти интервали типо из базы
+        // p.s. Високосный год не учитываю
         return [
-            new StartTravelDateInterval(
-                start: new DateTime("01-03-$currentYear"),
-                end: new DateTime("01-02-$nextYear"),
-                paymentDateIntervals: [
 
+            new StartTravelDateInterval(
+                start: new DateTime("01-04-$nextYear"),
+                end: new DateTime("30-09-$nextYear"),
+                paymentDateIntervals: [
+                    new PaymentDateInterval(
+                        start: new DateTime("01-01-$currentYear"),
+                        end: new DateTime("30-11-$currentYear"),
+                        discountProcent: 7
+                    ),
+                    new PaymentDateInterval(
+                        start: new DateTime("30-11-$currentYear"),
+                        end: new DateTime("31-12-$currentYear"),
+                        discountProcent: 5
+                    ),
+                    new PaymentDateInterval(
+                        start: new DateTime("01-01-$nextYear"),
+                        end: new DateTime("31-01-$nextYear"),
+                        discountProcent: 3
+                    ),
                 ],
-            )
+            ),
+
+            new StartTravelDateInterval(
+                start: new DateTime("01-10-$currentYear"),
+                end: new DateTime("14-01-$nextYear"),
+                paymentDateIntervals: [
+                    new PaymentDateInterval(
+                        start: new DateTime("01-01-$currentYear"),
+                        end: new DateTime("29-03-$currentYear"),
+                        discountProcent: 7
+                    ),
+                    new PaymentDateInterval(
+                        start: new DateTime("29-03-$currentYear"),
+                        end: new DateTime("30-04-$currentYear"),
+                        discountProcent: 5
+                    ),
+                    new PaymentDateInterval(
+                        start: new DateTime("30-04-$currentYear"),
+                        end: new DateTime("31-05-$currentYear"),
+                        discountProcent: 3
+                    ),
+                ]
+            ),
+
+            new StartTravelDateInterval(
+                start: new DateTime("15-01-$nextYear"),
+                end: new DateTime('01-01-2099'),
+                paymentDateIntervals: [
+                    new PaymentDateInterval(
+                        start: new DateTime("01-01-$currentYear"),
+                        end: new DateTime("29-08-$currentYear"),
+                        discountProcent: 7
+                    ),
+                    new PaymentDateInterval(
+                        start: new DateTime("29-08-$currentYear"),
+                        end: new DateTime("30-09-$currentYear"),
+                        discountProcent: 5
+                    ),
+                    new PaymentDateInterval(
+                        start: new DateTime("30-09-$currentYear"),
+                        end: new DateTime("31-10-$currentYear"),
+                        discountProcent: 3
+                    ),
+                ]
+            ),
         ];
     }
 }
