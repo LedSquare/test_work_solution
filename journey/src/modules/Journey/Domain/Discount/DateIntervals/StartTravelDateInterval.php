ee8c19aa9dd2693aff3e2d8c5d5142500a1ff9f9
LedSquare <exilelovich@yandex.ru>
2024-10-31 16:36:46 +0300

    Fea: Added Map Request Payload. Updated: StartTravelDateInterval - added validation

--- journey/src/modules/Journey/Domain/Discount/DateIntervals/StartTravelDateInterval.php
+++ journey/src/modules/Journey/Domain/Discount/DateIntervals/StartTravelDateInterval.php
@@ -2,6 +2,8 @@
 
 namespace App\modules\Journey\Domain\Discount\DateIntervals;
 
+use App\modules\Journey\Domain\Discount\DateIntervals\PaymentDateInterval;
+use App\modules\Journey\Domain\Exceptions\Discount\DiscountDateException;
 use DateTime;
 
 final readonly class StartTravelDateInterval
@@ -14,5 +16,17 @@ final readonly class StartTravelDateInterval
          */
         public array $paymentDateIntervals,
     ) {
+        $this->validate();
+    }
+
+    public function validate(): void
+    {
+        if ($this->start >= $this->end) {
+            throw new DiscountDateException('The start date of the interval is greater than the end date', 422);
+        }
+
+        if (!$this->paymentDateIntervals[0] instanceof PaymentDateInterval) {
+            throw new \InvalidArgumentException('Wrong payment date type', 400);
+        }
     }
 }
--- journey/src/modules/Journey/Infrastructure/Symfony/API/Controller/CostCalculationWithDiscountController.php
+++ journey/src/modules/Journey/Infrastructure/Symfony/API/Controller/CostCalculationWithDiscountController.php
@@ -4,13 +4,11 @@ namespace App\modules\Journey\Infrastructure\Symfony\API\Controller;
 
 use App\modules\Journey\Application\DTORequest\CostCalculationRequestDTO;
 use App\modules\Journey\Application\Service\DiscountCostCalculationServiceInterface;
-use App\modules\Journey\Infrastructure\Symfony\API\Request\CostCalculationWithDiscountValidationRequest;
-use DateTime;
+use App\modules\Journey\Infrastructure\Symfony\API\Request\CostCalculationWithDiscountRequest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class CostCalculationWithDiscountController extends AbstractController
 {
@@ -20,28 +18,18 @@ class CostCalculationWithDiscountController extends AbstractController
     }
 
     #[Route(path: 'discount_calculation', name: 'journey_discount_calculation', methods: 'POST')]
-    public function index(Request $request, ValidatorInterface $validator): JsonResponse
-    {
-        $data = json_decode($request->getContent(), true);
-
-        $request = new CostCalculationWithDiscountValidationRequest(
-            baseCost: $data['base_cost'],
-            birthDate: new DateTime($data['birth_date']),
-            startTravelDate: new DateTime($data['start_travel_date']),
-            paymentDate: new DateTime($data['payment_date']),
-        );
-
-        $validator->validate($request);
+    public function index(
+        #[MapRequestPayload] CostCalculationWithDiscountRequest $request
+    ): JsonResponse {
 
         $dto = new CostCalculationRequestDTO(
-            baseCost: $request->baseCost,
-            birthDate: $request->birthDate,
-            startTravelDate: $request->startTravelDate,
-            paymentDate: $request->paymentDate,
+            baseCost: $request->base_cost,
+            birthDate: $request->birth_date,
+            startTravelDate: $request->start_travel_date,
+            paymentDate: $request->payment_date,
         );
 
         $responseDto = $this->discountCostCalculationService->calculate($dto);
-
         return $this->json(data: $responseDto);
     }
 }
